94a7c6f96112381f460ffa34a767000b1d271c34
sokrat165 <anaesmyma7mad2@gmail.com>
2026-02-15 21:02:59 +0200

    chore(crud): remove deprecated CRUD modules (baseitem, baseregister, baserepository, etc.)

--- src/crud/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/crud/__init__.py
-from .basestudent import StudentRepository
-from .baseitem import ItemRepository
-from .baseregister import UserRepository
-
-__all__ = ["StudentRepository", "ItemRepository", "UserRepository"]
