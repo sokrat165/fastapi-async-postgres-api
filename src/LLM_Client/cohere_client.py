8aaec16e934cf2f30fa070a53bbd12fe5735091c
sokrat165 <anaesmyma7mad2@gmail.com>
2026-02-17 12:34:30 +0200

    refactor: remove deprecated Cohere client and pass_hash service

--- src/LLM_Client/cohere_client.py
+++
@@ -1,9 +0,0 @@
-# src/LLM_Client/cohere_client.py
-from cohere import ClientV2
-import cohere
-from src.core.config import Settings
-
-
-def get_co_client():
-    return cohere.AsyncClientV2(api_key=Settings().API_KEY)
-
